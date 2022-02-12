75f93f9b925ba5f9311edf10dd35352eaf1b5f69
unknown <chaouche-amir@yahoo.fr>
2022-02-12 00:41:03 -0500

    Aek

---
+++ Algorithm/C++/Myfristprogram.cpp
@@ -0,0 +1,14 @@
+// C++ program to display "Hello World"
+
+// Header file for imput output functions 
+#include <iostream>
+using namespace std;
+
+// Main() function : where the excution of the program begins
+int main() {
+
+    // Print hello world
+    cout << "Hello World";
+
+    return 0;
+}
---
+++ Algorithm/C++/Variable.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+
+/*  Declaring a singale variable :
+type  variale_name;  */
+
+char B = 'A';
+
+/* Declaring multiple variables :
+type variable_name, variable2_name, variable3_name  */ 
+
+int x = 30, y = 30, z = 40;
+
+cout << x + y + z <<endl;
+return 0;
+
+
+
+}
+
+
