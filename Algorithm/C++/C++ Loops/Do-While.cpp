7e060c1d1ed8c1e1b2bda1d4d612c611076f3462
unknown <chaouche-amir@yahoo.fr>
2022-02-27 00:22:58 -0500

    aek

---
+++ Algorithm/C++/C++ Loops/Do-While.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    /* do {
+    code block to be executed
+  }
+  while (condition);  */
+  
+    int i = 0;
+    do {
+        cout << i << "\n";
+        i++;
+    }
+
+    while (i < 5);
+    {
+        /* cou */
+    }
+
+return 0;
+    
+}
---
+++ Algorithm/C++/C++ Loops/While Loop.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 0;
+    
+    /*
+    while ( condition )
+    {
+         code 
+    }
+     
+    */
+   
+   // In the example below, the code in the loop will run, over and over again, as long as a variable (i) is less than 5:
+    while (i < 5)
+     {
+        cout << i << "\n";
+        i++;
+
+    }
+
+return 0;
+    
+    
+}
