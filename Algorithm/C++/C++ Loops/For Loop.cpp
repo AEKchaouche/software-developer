8f1440bba719ea065900ee9e6183970ba8d80c57
AEKchaouche <43480300+AEKchaouche@users.noreply.github.com>
2022-02-28 04:58:09 -0500

    Update For Loop.cpp

--- Algorithm/C++/C++ Loops/For Loop.cpp
+++ Algorithm/C++/C++ Loops/For Loop.cpp
@@ -6,8 +6,7 @@ int main() {
     for ( int i = 2; i < 10; i = i + 2) {
         cout << i << "\n";
     }
-
-    return 0;
+return 0;
 
     /* for (statement 1; statement 2; statement 3) {
          code block to be executed
