4eb03cc3cc31ab766a97ebd9f0b14e42f2849d6a
unknown <chaouche-amir@yahoo.fr>
2022-02-28 01:08:38 -0500

    aek

---
+++ Algorithm/C++/C++ Loops/For Loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    for ( int i = 2; i < 10; i = i + 2) {
+        cout << i << "\n";
+    }
+
+    return 0;
+
+    /* for (statement 1; statement 2; statement 3) {
+         code block to be executed
+    }
+    */
+
+}
