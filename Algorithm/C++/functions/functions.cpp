5c6ccfb8314699b56b53794268dd27bb1de72081
unknown <chaouche-amir@yahoo.fr>
2022-03-09 22:36:46 -0500

    aa

---
+++ Algorithm/C++/functions/functions.cpp
@@ -0,0 +1,21 @@
+# include <iostream>
+using namespace std;
+
+
+
+/* Syntax
+void myFuction() {               myFunction() is the name of the function
+    code to be executed          void means that the function does not have a return value
+}
+*/
+
+void myFunction() {
+    cout << "I just got executed";
+}
+
+int main() {
+    myFunction();
+    return 0;
+}
+
+
