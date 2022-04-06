1b0cf680b102ce14ed29a258ffaf590695ecb4a7
unknown <chaouche-amir@yahoo.fr>
2022-04-06 01:06:27 -0400

    aek

---
+++ Algorithm/C++/Arrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// To declare an array, define the variable type, specify the name of the array followed by square brackets and specify the number of elements it should store:
+
+// string cars[4];
+
+int main () {
+    string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+    cout <<cars[2];
+
+return 0;
+
+}
