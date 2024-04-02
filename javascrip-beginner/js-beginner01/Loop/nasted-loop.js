eddb09ec3cc68eddf7cd1deb5c255cb0cfcdf996
unknown <chaouche-amir@yahoo.fr>
2024-04-02 00:08:20 -0400

    js

--- javascrip-beginner/js-beginner01/Loop/nasted-loop.js
+++ javascrip-beginner/js-beginner01/Loop/nasted-loop.js
@@ -1,16 +1,20 @@
-
-
-let cars = ['BMW' , 'Mercedes', 'Honda' ];
+let cars = ["BMW", "Mercedes", "Honda"];
 let models = [2020, 2021, 2022];
+let colors= ["Black", "Red", "Green"]
 
-for (let i = 0; i < cars.length; i++)
-{
-    console.log(`Car: ${cars[i]}`);
-    for (j = 0 ; j < models.length; j++);
-    {
+
+for (let i = 0; i < cars.length; i++) {
+    console.log(`Cars : ${cars[i]}`);
+    for (let j = 0 ; j < models.length; j++)
+     {
         console.log(`Models: ${models[j]}`);
     }
+    for (let y = 0; y < colors.length; y++)
+    {
+        console.log(`Colors: ${colors[y]}`);
+    }
 
-    console.log('___________')
+    console.log('______________________')
+    
 }
 
