47bf4fd464f9e08666649ceeacdbc1246a18f742
unknown <chaouche-amir@yahoo.fr>
2024-04-08 01:12:21 -0400

    js

--- javascrip-beginner/js-beginner01/Loop/continue-statement.js
+++ javascrip-beginner/js-beginner01/Loop/continue-statement.js
@@ -1,9 +1,9 @@
 let content = " ";
 let i;
 for (i = 1; i < 7; i++) {
-    if (i === 4) {
-        continue;
+    if (i === 3 || i === 4) {
+        continue
     }
-    content += "Geeks" + i + "\n" ;
+    content += "The number :" + i + "\n"
 }
 console.log(content);
