8fc2c108091dda067438aa10d25dacdc241832a8
unknown <chaouche-amir@yahoo.fr>
2024-05-02 23:43:56 -0400

    js

---
+++ javascrip-beginner/js-beginner01/function/Calling-Functions.js
@@ -0,0 +1,9 @@
+function welcomeMsg(name) {
+    return ("hello " + name + " welcome to javascript");
+}
+
+// creating a variable
+let nameVal = "Admin";
+
+// calling the function
+console.log(welcomeMsg(nameVal));
