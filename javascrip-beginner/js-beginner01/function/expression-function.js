074f944ac74ff6aa2ac527f6b693fc2e50189b71
unknown <chaouche-amir@yahoo.fr>
2024-05-03 00:10:31 -0400

    js

---
+++ javascrip-beginner/js-beginner01/function/expression-function.js
@@ -0,0 +1,6 @@
+function add(x, y) {
+    let z = x + y ;
+    return z ;
+}
+
+console.log("Addition : " + add(5, 4))
---
+++ javascrip-beginner/js-beginner01/function/return-statement.js
@@ -0,0 +1,9 @@
+function Product(a, b) {
+    // Return the product of a and b
+    return a * b;
+}
+
+console.log(Product(6, 10));
+
+/* The return statement  is used to end the execution of a 
+function and specifies the value to be returned by that function */
