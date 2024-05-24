e2f0aacbe049ec794f51c5a1aea1a70965b64156
unknown <chaouche-amir@yahoo.fr>
2024-05-24 00:44:16 -0400

    hoisting js

---
+++ javascrip-beginner/js-beginner01/Function-Definitions/hoisting.js
@@ -0,0 +1,8 @@
+
+/*hoisting is a mechanism where variable and 
+function declarations are moved to the top */ 
+
+sayHello() // Output : "Hello world!"
+function sayHello() {
+    console.log("Hello, world !")
+}
