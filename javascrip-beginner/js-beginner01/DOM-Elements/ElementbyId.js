22ee197ff131e136eaab50a74a4755d43cc4ecc8
unknown <chaouche-amir@yahoo.fr>
2024-06-27 00:06:47 -0400

    s

---
+++ javascrip-beginner/js-beginner01/DOM-Elements/ElementbyId.js
@@ -0,0 +1,11 @@
+function changeColor() {
+    //Selecting element by ID
+    let myDiv = document.getElementById("myElement");
+
+    // Manipulating the selected element
+    myDiv.style.color = "red";
+
+
+
+    
+}
