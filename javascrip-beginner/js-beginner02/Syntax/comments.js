2c50382848deb1e2f1759cc6dcda7e8d2c47a2b2
unknown <chaouche-amir@yahoo.fr>
2025-09-25 00:31:06 -0400

    a

---
+++ javascrip-beginner/js-beginner02/Syntax/comments.js
@@ -0,0 +1,10 @@
+
+// Single Line Comments
+ let d = 3 ;
+
+ // Multi-line comments 
+
+ /*  The code below will change
+the heading with id = "myH"
+and the paragraph with id = "myP"
+in my web page:  */
---
+++ javascrip-beginner/js-beginner02/Syntax/syntax.js
@@ -0,0 +1,14 @@
+
+// How to declare variable
+
+let x = 5;
+let y = 6;
+
+// How to compute values
+let z = x + y
+
+console.log(z)
+
+
+
+
