d064570781c610cb8b2e4f8b8ad31686058fff66
unknown <chaouche-amir@yahoo.fr>
2025-10-03 23:34:26 -0400

    a

---
+++ javascrip-beginner/js-beginner02/Operators/Arithmetic.js
@@ -0,0 +1,45 @@
+
+// The addition operator (+) adds numbers:
+let number01 = 5;
+let number02 = 2;
+let a = number01 + number02;
+
+console.log(number01 + number02);
+
+// The subtraction operator (-) subtracts numbers.
+
+let number03 = 10;
+let number04 = 6;
+
+console.log(number03 + number04);
+
+// The division operator (/) divides numbers.
+
+let number05 = 24;
+let number06 = 6;
+
+console.log(number05 / number06);
+
+// The modulus operator (%) returns the division remainder.
+let num01 = 5;
+let num02 = 2;
+ console.log(num01 % num02);
+
+ // The increment operator (++) increments numbers.
+ let num03 = 5
+ num03++
+ let k = num03;
+
+ console.log(num03);
+
+ // The decrement operator (--) decrements numbers.
+ let num04 = 5
+ num04--;
+ let f = num03;
+
+ console.log(num04);
+
+ // The exponentiation operator (**) raises the first operand to the power of the second operand.
+ let num05 = 5;
+ let d = num05 ** 2;
+ console.log(d);
---
+++ javascrip-beginner/js-beginner02/Operators/assgnment.js
@@ -0,0 +1,35 @@
+
+// JavaScript Assignment Operators
+
+// Addition Assignment
+let x = 10 ;;
+x += 5;
+
+console.log(x)
+
+// Subtraction Assignment  
+let y = 20 ;
+y -= 10 ;
+
+console.log(y)
+
+// Exponentiation Assignment
+let a1 = 10;
+a1 **= 5;
+console.log(a1)
+
+// The Division Assignment Operator 
+let b1 = 10;
+b1 /= 5;
+console.log(b1)
+
+// String Assignment
+// Addition Assignment
+let text = 'Hello';
+text += " World";
+console.log(text)
+
+// The Logical AND assignment operator &&
+let k1 = true;
+let f1 = k1 &&= 10;
+console.log(f1)
