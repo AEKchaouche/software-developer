ac30d2452ae48b03d773b64d715c7a4828bd94fb
unknown <chaouche-amir@yahoo.fr>
2025-10-09 22:15:01 -0400

    e

---
+++ javascrip-beginner/js-beginner02/Operators/Comparison.js
@@ -0,0 +1,10 @@
+
+// The == (equal) Operator : copre vlue only
+let x7 = 5;
+
+console.log(x7 == 8); // false
+console.log(10 == "10") // true ; even "10" is string 
+
+// The === (strict equal) Operator : Compare vlue and type 
+console.log(6 === 6); // true
+console.log(6 === "6"); // false
